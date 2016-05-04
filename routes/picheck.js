6bd21f020418aa8a68a862585c904191b4af0c10
Alex4J <monkeycoin@protonmail.ch>
2016-05-04 20:00:03 +0200

    adding new response to pifinder

--- routes/picheck.js
+++ routes/picheck.js
@@ -7,7 +7,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.send("ok");
+    res.send("ok"+req.query.ip);
 });
 
 module.exports = router;
