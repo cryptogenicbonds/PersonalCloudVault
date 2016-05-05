1a8dc627fdbb07d1fc85875f6707ccb0823931cd
Alex4J <monkeycoin@protonmail.ch>
2016-05-05 17:16:48 +0200

    changing startup script

--- startup.sh
+++ startup.sh
@@ -1,2 +1,2 @@
 /bin/su -c "nohup /usr/local/bin/node /home/pi/PersonalCloudVault/bin/www > /home/pi/pcv.log 2>&1&" -s /bin/sh pi
-/bin/su -c "echo $! > /home/pi/pcv.pid" -s /bin/sh
+/bin/su -c "echo $! > /home/pi/pcv.pid" -s /bin/sh pi
